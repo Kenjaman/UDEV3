a8a78f08399a7f08eefe67592f5689b068fb9f2e
Kenjaman <56792437+Kenjaman@users.noreply.github.com>
2019-10-22 13:37:38 +0200

    Monopoly

---
+++ Monopoly/src/monopoly/core/Dice.java
@@ -0,0 +1,91 @@
+package monopoly.core;
+
+import java.util.Random;
+
+public class Dice {
+	private int value;
+	private int nbFace;
+	private Random rdn = new Random();
+
+	public Dice(int nbFaces) {
+		if(nbFaces<4) {
+			throw new IllegalArgumentException("Un dé possède au moins 4 faces");
+		}
+		this.nbFace=nbFaces;
+		this.lancer();
+	}
+
+	public void lancer() {
+		value = rdn.nextInt(this.nbFace)+1;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void afficher() {
+		switch (this.value) {
+		case 1: {
+			System.out.println(
+					"┌───────┐\n"
+							+"│       │\n"
+							+"│   ●   │\n"
+							+"│       │\n"
+							+"└───────┘");
+			break;
+		}
+
+		case 2: {
+			System.out.println(
+					"┌───────┐\n"
+							+"│ ●     │\n"
+							+"│       │\n"
+							+"│     ● │\n"
+							+"└───────┘");
+			break;
+		}
+
+		case 3: {
+			System.out.println(
+					"┌───────┐\n"
+							+"│     ● │\n"
+							+"│   ●   │\n"
+							+"│ ●     │\n"
+							+"└───────┘");
+			break;
+		}
+
+		case 4: {
+			System.out.println(
+					"┌───────┐\n"
+							+"│ ●   ● │\n"
+							+"│       │\n"
+							+"│ ●   ● │\n"
+							+"└───────┘");
+			break;
+		}
+
+		case 5: {
+			System.out.println(
+					"┌───────┐\n"
+							+"│ ●   ● │\n"
+							+"│   ●   │\n"
+							+"│ ●   ● │\n"
+							+"└───────┘");
+			break;
+		}
+
+		case 6: {
+			System.out.println(
+					"┌───────┐\n"
+							+"│ ●   ● │\n"
+							+"│ ●   ● │\n"
+							+"│ ●   ● │\n"
+							+"└───────┘");
+			break;
+		}
+
+
+		}
+	}
+}
