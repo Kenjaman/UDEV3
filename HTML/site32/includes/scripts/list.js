6dfd15c0ecd4d79fa10bc348c1fdc9b74d243f0e
Kenjaman <56792437+Kenjaman@users.noreply.github.com>
2019-12-18 22:59:56 +0100

    js netflix to finish

--- HTML/site32/includes/scripts/list.js
+++ HTML/site32/includes/scripts/list.js
@@ -5,7 +5,9 @@ function getListeSeries(){
 			var text= this.responseText;
 			var lesSeries = JSON.parse(text);
 			var liSeries=document.getElementById("bodyTableauSeries");
+
 			for(var i=0;i<lesSeries.length;i++){
+
 				var row = liSeries.insertRow(i);
 				var cell1 = row.insertCell(0);
 				var cell2 = row.insertCell(1);
@@ -13,7 +15,9 @@ function getListeSeries(){
 				cell1.innerText = lesSeries[i].nom;
 				cell2.innerText = lesSeries[i].anneeparution;
 				getNbSaisons(lesSeries[i].id,cellSaison);
+				row.setAttribute("class","ligneTab");
 				row.setAttribute("tag", lesSeries[i].id);
+				liSeries.append(row);
 			}
 		}
 		else {
@@ -22,7 +26,7 @@ function getListeSeries(){
 	};
 	xmlhttp.open("GET", "https://www.devatom.net/formation/UDEV3/APINetflix/api.php?data=series");
 	xmlhttp.send();
-};
+}
 
 function getNbSaisons(id, cell){
 	var nbSaison =0;
@@ -42,3 +46,52 @@ function getNbSaisons(id, cell){
 
 }
 
+
+function getListeSaisons(tableSaisons, idSerie) {
+	var xmlhttp = getAjaxRequestObject();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var text= this.responseText;
+			var lesSaisons = JSON.parse(text);
+			var liSaisons=tableSaisons;
+			for(var i=0;i<lesSaisons.length;i++){
+
+				var row = liSaisons.insertRow(i);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				var cellSaison = row.insertCell(2);
+				cell1.innerText = lesSaisons[i].numero;
+				cell2.innerText = lesSaisons[i].annee_diffusion;
+				getNbEpisodes(lesSaisons[i].id,cellSaison);
+				row.setAttribute("class","ligneTabSaison");
+				row.setAttribute("tag", lesSaisons[i].id);
+				liSaisons.append(row);
+			}
+		}
+		else {
+			console.log("je n'ai pas mon fichier" + this.responseText);
+		}
+	};
+	xmlhttp.open("GET", "https://www.devatom.net/formation/UDEV3/APINetflix/api.php?data=saisons&idserie="+idSerie);
+	xmlhttp.send();
+}
+
+function getNbEpisodes(id, cellSaison) {
+	var nbEpisodes =0;
+	var xmlHttpSaisons = getAjaxRequestObject();
+	xmlHttpSaisons.onreadystatechange = function () {
+		if(this.readyState === 4 && this.status === 200){
+			try{
+				nbEpisodes = JSON.parse(this.responseText).length;
+				console.log(nbEpisodes);
+			}catch (e) {
+				nbEpisodes=0;
+			}
+			cellSaison.innerText=nbEpisodes;
+		}
+	};
+	console.log("https://www.devatom.net/formation/UDEV3/APINetflix/api.php?data=episodes&idsaison="+id);
+	xmlHttpSaisons.open("GET", "https://www.devatom.net/formation/UDEV3/APINetflix/api.php?data=episodes&idsaison="+id);
+	xmlHttpSaisons.send();
+
+}
